1150f281029291a62a41b88814b1ebea6f9aa077
satsuya0114 <satsuya0114@hotmail.com.tw>
2016-06-10 15:45:05 +0800

    verson3

--- test3/mainwindow.cpp
+++ test3/mainwindow.cpp
@@ -8,17 +8,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //setStyleSheet("background:white;");
+    QAction *smallWindow = new QAction("&Show Window",this);
     QAction *qu = new QAction("&Quit",this);
     setFixedSize(510,900);
     setWindowTitle(tr("Context Menu Show 1"));
      //add QActions
-    addAction(new QAction(tr("&Open"), this));
+    /*addAction(new QAction(tr("&Open"), this));
     addAction(new QAction(QIcon(":/images/mark.png"), tr("&Mark"), this));
+    addAction(smallWindow);
     addAction(qu);
     //設定 contextMenuPolicy屬性值 以Actions為context menu
     setContextMenuPolicy(Qt::ActionsContextMenu);
-    connect(qu,SIGNAL(triggered()),qApp,SLOT(quit()));
+    connect(smallWindow,SIGNAL(triggered()),qApp,SLOT(OpenNewWindow()));
+    connect(qu,SIGNAL(triggered()),qApp,SLOT(quit()));*/
+    createActions();
+    setWindowFlags(Qt::WindowStaysOnTopHint);
 
     koe = new QMediaPlayer();
     koe->setMedia(QUrl("qrc:/sounds/kc50v4.mp3"));
@@ -30,6 +34,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+/*void MainWindow::OpenNewWindow()
+{
+    /*CLISupport clisupport;
+    clisupport.setWindowModality(Qt::ApplicationModal);
+    clisupport.show();*/
+    /*Dialog dialog;
+    dialog.setModal(true);
+    dialog.show();
+    dialog.exec();
+
+}*/
 
 void MainWindow::mousePressEvent(QMouseEvent* event)
 {
@@ -59,4 +75,52 @@ void MainWindow::mouseMoveEvent(QMouseEvent* event)
 
 }
 
+void MainWindow::createActions()
+{
+    openSecWindowAct = new QAction(tr("&Support Start"),this);
+    openSecWindowAct->setStatusTip(tr("Open the support window,"
+                                      "and begin to support when you type comments on terminal"));
+    connect(openSecWindowAct,SIGNAL(triggered()),this,SLOT(openSecWindow()));
+
+    exitAct = new QAction(tr("&Exit"),this);
+    exitAct->setStatusTip(tr("Exit this program"));
+    connect(exitAct,SIGNAL(triggered()),this,SLOT(exit_()));
+}
+
+void MainWindow::openSecWindow()
+{
+    /*Dialog dialog;
+    dialog.setModal(true);
+    dialog.show();
+    dialog.exec();*/
+    /*CLISupport clisupport;
+    clisupport.setWindowModality(Qt::ApplicationModal);
+    clisupport.show();*/
+    if(!clisupport->isVisible()){
+    clisupport = new CLISupport();
+    //clisupport->setWindowFlags(Qt::WindowStaysOnTopHint);
+    clisupport->show();
+    }
+
+
+
+}
+
+void MainWindow::exit_()
+{
+    if(clisupport->isVisible())
+    {
+        clisupport->close();
+    }
+    close();
+}
+
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    QMenu menu(this);
+    menu.addAction(openSecWindowAct);
+    menu.addAction(exitAct);
+    menu.exec(event->globalPos());
+
+}
 
--- test3/mainwindow.h
+++ test3/mainwindow.h
@@ -5,6 +5,9 @@
 #include <QPoint>
 #include <QMouseEvent>
 #include <QMediaPlayer>
+#include <QMenu>
+#include <QAction>
+#include"clisupport.h"
 namespace Ui {
 class MainWindow;
 }
@@ -19,6 +22,7 @@ public:
 protected:
     void mouseMoveEvent(QMouseEvent* event);
     void mousePressEvent(QMouseEvent* event);
+    void contextMenuEvent(QContextMenuEvent *event);
 
 private:
     Ui::MainWindow *ui;
@@ -26,6 +30,15 @@ private:
     QPoint mousePos;
     QPoint dPos;
     QMediaPlayer * koe;
+    void createActions();
+
+    QAction *openSecWindowAct;
+    QAction *exitAct;
+    CLISupport *clisupport;
+private slots:
+    void openSecWindow();
+    void exit_();
+
 
 };
 
